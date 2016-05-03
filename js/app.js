6dc7ebfc6355bddf8ea5793b16f2d4c7ec607be9
Zachary Liu <liu.zachary@gmail.com>
2016-05-03 01:14:35 -0400

    Add initial template (modified from https://github.com/mozilla/mortar-game-stub)

---
+++ js/app.js
@@ -0,0 +1,156 @@
+
+window.onload = function() {
+
+    // A cross-browser requestAnimationFrame
+    // See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
+    var requestAnimFrame = (function() {
+        return window.requestAnimationFrame    ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(callback){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    // Create the canvas
+    var mainContainer = document.querySelector('main');
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    var initialCanvasWidth = canvas.width = 320;
+    var initialCanvasHeight = canvas.height = 480;
+    mainContainer.appendChild(canvas);
+
+    // The player's state
+    var player = {
+        x: 0,
+        y: 0,
+        sizeX: 30,
+        sizeY: 30
+    };
+
+    // Don't run the game when the tab isn't visible
+    window.addEventListener('focus', function() {
+        unpause();
+    });
+
+    window.addEventListener('blur', function() {
+        pause();
+    });
+
+    window.addEventListener('resize', resize);
+
+    //Initially resize the game canvas.
+    resize();
+    // Let's play this game!
+    reset();
+    var then = Date.now();
+    var running = true;
+    main();
+
+
+    // Functions ---
+
+
+    // Reset game to original state
+    function reset() {
+        player.x = 0;
+        player.y = 0;
+    }
+
+    // Pause and unpause
+    function pause() {
+        running = false;
+    }
+
+    function unpause() {
+        running = true;
+        then = Date.now();
+        main();
+    }
+
+    // Update game objects.
+    // We'll use GameInput to detect which keys are down.
+    // If you look at the bottom of index.html, we load GameInput
+    // from js/input.js right before app.js
+    function update(dt) {
+        // Speed in pixels per second
+        var playerSpeed = 100;
+
+        if(GameInput.isDown('DOWN')) {
+            // dt is the number of seconds passed, so multiplying by
+            // the speed gives you the number of pixels to move
+            player.y += playerSpeed * dt;
+        }
+
+        if(GameInput.isDown('UP')) {
+            player.y -= playerSpeed * dt;
+        }
+
+        if(GameInput.isDown('LEFT')) {
+            player.x -= playerSpeed * dt;
+        }
+
+        if(GameInput.isDown('RIGHT')) {
+            player.x += playerSpeed * dt;
+        }
+
+        // You can pass any letter to `isDown`, in addition to DOWN,
+        // UP, LEFT, RIGHT, and SPACE:
+        // if(GameInput.isDown('a')) { ... }
+    }
+
+    // Draw everything
+    function render() {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = 'green';
+        ctx.fillRect(player.x, player.y, player.sizeX, player.sizeY);
+    }
+
+    // The main game loop
+    function main() {
+        if(!running) {
+            return;
+        }
+
+        var now = Date.now();
+        var dt = (now - then) / 1000.0;
+
+        update(dt);
+        render();
+
+        then = now;
+        requestAnimFrame(main);
+    }
+
+    // based on: https://hacks.mozilla.org/2013/05/optimizing-your-javascript-game-for-firefox-os/
+    function resize() {
+        var browser = [
+            window.innerWidth,
+            window.innerHeight
+        ];
+        // Minimum scale
+        var scale = Math.min(
+            browser[0] / initialCanvasWidth,
+            browser[1] / initialCanvasHeight);
+        // Scaled content size
+        var size = [
+            initialCanvasWidth * scale,
+            initialCanvasHeight * scale
+        ];
+        // Offset from top/left
+        var offset = [
+            (browser[0] - size[0]) / 2,
+            (browser[1] - size[1]) / 2
+        ];
+
+        // Apply CSS transform
+        var rule = "translate(" + offset[0] + "px, " + offset[1] + "px) scale(" + scale + ")";
+        mainContainer.style.transform = rule;
+        mainContainer.style.webkitTransform = rule;
+    }
+
+};

0cc2e2fa04a69a82bc323d7d007136b82f257bbd
Zachary Liu <liu.zachary@gmail.com>
2016-05-05 13:01:35 -0400

    Fix rendering dimensions on retina displays

--- js/renderer.js
+++ js/renderer.js
@@ -7,12 +7,11 @@ var Renderer = function (canvas) {
   document.body.appendChild(this.stats.domElement);
 
   this.renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, preserveDrawingBuffer: true});
-  this.renderer.setPixelRatio(window.devicePixelRatio);
   this.renderer.setClearColor(0x444444);
 
   // this.camera = new THREE.PerspectiveCamera(35, canvas.width / canvas.height, 1, 10000);
-  this.camera = new THREE.OrthographicCamera(canvas.width / -2, canvas.width / 2, canvas.height / 2, canvas.height / -2, 1, 1000);
-  this.camera.position.set(canvas.width / 2, canvas.height / 2, -100);
+  this.camera = new THREE.OrthographicCamera(GameState.WIDTH / -2, GameState.WIDTH / 2, GameState.HEIGHT / 2, GameState.HEIGHT / -2, 1, 1000);
+  this.camera.position.set(GameState.WIDTH / 2, GameState.HEIGHT / 2, -100);
   this.camera.rotation.set(Math.PI, 0, 0);
 
   // this.cameraControls = new THREE.TrackballControls(this.camera, canvas);
@@ -37,6 +36,7 @@ Renderer.prototype.resize = function (width, height) {
   // Camera position updates on its own with orthographic perspective
   // this.camera.position.x = width / 2;
   // this.camera.position.y = height / 2;
-  
+
+  this.renderer.setPixelRatio(window.devicePixelRatio);
   this.renderer.setSize(width, height);
 };

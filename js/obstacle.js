8aeb662ac9b51cea56125c4dc15a845dea3d6d75
Zachary Liu <liu.zachary@gmail.com>
2016-05-09 11:00:15 -0400

    Apply loop subdivision to obstacles

--- js/obstacle.js
+++ js/obstacle.js
@@ -2,7 +2,8 @@ var Obstacle = function (opts) {
   this.type = 'obstacle';
 
   opts = opts || {};
-  var geometry = new THREE.BoxGeometry( 10, opts.height || 10, opts.width || 10 );
+  var geometry = new THREE.BoxGeometry( 10, opts.height || 10, opts.width || 10, 3, 3, 3 );
+  this._modifier.modify(geometry);
   this.color = opts.color || Math.random() * 0xffffff;
   var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: this.color } ) );
 
@@ -10,6 +11,8 @@ var Obstacle = function (opts) {
 };
 
 Obstacle.prototype = Object.create(GameObject.prototype);
+
+Obstacle.prototype._modifier = new THREE.SubdivisionModifier(2);
 
 Obstacle.prototype.handleCollision = function (intersection) {
   return (intersection.color == this.color) ? Collider.CollisionBehavior.PASS : Collider.CollisionBehavior.ABSORB;

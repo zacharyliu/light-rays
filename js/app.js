8d4034189e179d8afd0f82f3bf1c33f2d899db78
Jonathan Zong <jonathan@jonathanzong.com>
2016-05-10 22:07:02 -0400

    menu

--- js/app.js
+++ js/app.js
@@ -2,6 +2,29 @@ window.onload = function() {
 
     // Create the canvas
     var mainContainer = document.querySelector('main');
-    
     var game = new Game(mainContainer);
+    game.pause();
+    $(mainContainer).hide();
+
+    $('intro button').click(function() {
+      $('intro').fadeOut(350, function() {
+        $(this).remove();
+        $(mainContainer).fadeIn(200);
+        game.unpause();
+      });
+      return false;
+    });    
+
+
+    function color_cycle() {
+      $('intro #title span').each(function() {
+        $(this).css('color', Please.make_color({
+            saturation: .7,
+            value: .7
+        }));
+      });
+    }
+    color_cycle();
+    setInterval(color_cycle, 3000);
+
 };
